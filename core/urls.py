e3109c3855382a2b980f3324e84fbc21e2a911e8
Jotamontiel <jorgemontielmontiel@gmail.com>
2020-07-04 22:01:56 -0400

    Actualizacion templates en general

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView, DisplayPageView
+from .views import HomePageView, AboutPageView, DisplayPageView, MapDisplayView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name="home"),
     path('home/<int:idcategory>', HomePageView.as_view(), name="home"),
     path('about/', AboutPageView.as_view(), name="about"),
     path('display/', DisplayPageView.as_view(), name="display"),
+    path('map_display/', MapDisplayView.as_view(), name="map_display"),
 ]
--- core/views.py
+++ core/views.py
@@ -6,6 +6,7 @@ from registration.models import Profile
 from capsules.models import Capsule
 import random
 import folium
+from IPython.display import IFrame, HTML, display
 
 class HomePageView(ListView):
     model = Category
@@ -15,7 +16,7 @@ class HomePageView(ListView):
     def get_context_data(self, **kwargs):
         context = super(HomePageView, self).get_context_data(**kwargs)
         context['course_list'] = Course.objects.all()
-        context['capsule_list'] = Capsule.objects.all()
+        #context['capsule_list'] = Capsule.objects.all()
 
         # Others Courses [Begin]
         if len(context['course_list']) < 3:
@@ -61,14 +62,22 @@ class HomePageView(ListView):
             context['categories_amount'] = 4
         # AmountActivation [Finish]
 
-        figure = folium.Figure()
-        m = folium.Map(width='100%',height='100%',location=[-33.4569397, -70.6482697], zoom_start=12, tiles='Stamen Toner')
-        m.add_to(figure)
-        for item in context['capsule_list']:
-            if item.lat and item.lng:
-                folium.Marker(location=[item.lat, item.lng], popup=item.name, icon=folium.Icon(icon='cloud')).add_to(m)
-        figure = figure._repr_html_()
-        context['map'] = figure
+        #figure = folium.Figure()
+        #m = folium.Map(width='100%',height='100%',location=[-33.4569397, -70.6482697], zoom_start=12, tiles='Stamen Toner')
+        #m.add_to(figure)
+        #for item in context['capsule_list']:
+        #    if item.lat and item.lng:
+        #        folium.Marker(location=[item.lat, item.lng], popup=item.name, icon=folium.Icon(icon='cloud')).add_to(m)
+        #figure = figure._repr_html_()
+        #figure.save('map.html')
+        #print(figure)
+        #mapWidth = 600
+        #mapHeight = 600
+        #srcdoc = m.HTML.replace('"', '&quot;')
+        #embed = HTML('<iframe srcdoc="{}" '
+        #     'style="width: {}px; height: {}px; display:block; width: 50%; margin: 0 auto; '
+        #     'border: none"></iframe>'.format(figure, mapWidth, mapHeight))
+        #context['map'] = embed
 
         return context
 
@@ -99,3 +108,23 @@ class AboutPageView(ListView):
 
 class DisplayPageView(TemplateView):
     template_name = "core/display.html"
+
+class MapDisplayView(TemplateView):
+    model = Capsule
+    template_name = "core/map_display.html"
+    context_object_name = 'capsule_list'
+
+    def get_context_data(self, **kwargs):
+        context = super(MapDisplayView, self).get_context_data(**kwargs)
+        context['capsule_list'] = Capsule.objects.all()
+
+        figure = folium.Figure()
+        m = folium.Map(width='100%',height='100%',location=[-33.4569397, -70.6482697], zoom_start=12, tiles='Stamen Toner')
+        m.add_to(figure)
+        for item in context['capsule_list']:
+            if item.lat and item.lng:
+                folium.Marker(location=[item.lat, item.lng], popup=item.name, icon=folium.Icon(icon='cloud')).add_to(m)
+        figure = figure.get_root().render()
+        context['my_map'] = figure
+
+        return context
